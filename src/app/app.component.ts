1e4c114744c4ec7f005bb05e4f949f394d341936
azizul arif <you@example.com>
2020-06-12 20:52:47 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title : string = 'Counter App';
+  count: number = 0;
+
+  handleIncrease = () => {
+    //assignment hint start
+    // if(this.count==10)
+    // {
+    //   this.count=this.count+15;
+    //asiignment hint end }
+    this.count=this.count+1;
+  }
+  handleDecrease = () =>{
+    this.count=this.count-1;
+  }
+  handleReset =() =>{
+    this.count=0;
+  }
 }
